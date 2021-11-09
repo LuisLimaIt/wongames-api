2d9bd538f1e96d98fda5723cb82741ea8b83c9f1
Luis Paulo de Lima Soares <pls.luis@gmail.com>
2021-11-09 09:41:57 -0300

    Inicialização do projeto / Criando a Collection Type - Categories

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'd0a0224d9fbbd80bcdd676d673741deb'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'd4261fa8-c861-4074-b6ac-62e819c2e4e8'
+};
